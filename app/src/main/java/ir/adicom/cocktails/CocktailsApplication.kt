abbefdad7370e6ceed33348afca681bef8c27bcf
adicomdotir <adicom.ir@gmail.com>
2022-10-02 17:30:47 +0330

    Init commit

---
+++ app/src/main/java/ir/adicom/cocktails/CocktailsApplication.kt
@@ -0,0 +1,6 @@
+package ir.adicom.cocktails
+
+import android.app.Application
+
+class CocktailsApplication : Application() {
+}
---
+++ app/src/main/java/ir/adicom/cocktails/CocktailsGameActivity.kt
@@ -0,0 +1,16 @@
+package ir.adicom.cocktails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.adicom.cocktails.databinding.ActivityCocktailsGameBinding
+
+class CocktailsGameActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCocktailsGameBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCocktailsGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
